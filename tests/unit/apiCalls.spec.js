872fe484b5fbe772d0eb02ef2840c560214f5896
Noah Gibson <noah.gibson99@gmail.com>
2020-07-25 14:32:52 -0600

    Add mock implementation of fetch

--- tests/unit/apiCalls.spec.js
+++ tests/unit/apiCalls.spec.js
@@ -1,7 +1,23 @@
 import { dailyImage, dailyImageOnDate } from '../../apiCalls';
 
 describe('Api Calls', () => {
+  const mockResponse = {
+    hdurl: 'image_url_string',
+    title: 'Nasa Image',
+    explanation: 'This is a really great photo of a moon.'
+  }
+
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      });
+    });
+  });
+
   it('Should run a test', () => {
     expect(true).toBe(true);
-  })
+  });
+
 });
