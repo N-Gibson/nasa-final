0349514d327ef9a5693c859599c44c19a80a003b
Noah Gibson <noah.gibson99@gmail.com>
2020-07-25 18:21:27 -0600

    Add test for not ok fetch

--- tests/unit/apiCalls.spec.js
+++ tests/unit/apiCalls.spec.js
@@ -16,8 +16,16 @@ describe('Api Calls', () => {
     });
   });
 
-  it('Should return a photo response if fetch is ok', () => {
+  it('Should return a response if fetch is ok', () => {
     const result = dailyImage();
     expect(result).resolves.toEqual(mockResponse);
+  });
+
+  it('Should throw an error if the response is not ok' , () => {
+    const response = 'There was an error fetching your photo';
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({ ok: false });
+    });
+    expect(dailyImage()).rejects.toEqual(response);
   });
 });
