241f44d5173bf135e646c7beaab0da2d3161e7aa
Noah Gibson <noah.gibson99@gmail.com>
2020-07-25 18:37:15 -0600

    Add test for response not ok

--- tests/unit/apiCalls.spec.js
+++ tests/unit/apiCalls.spec.js
@@ -1,11 +1,11 @@
 import { dailyImage, dailyImageOnDate } from '../../apiCalls';
 
-describe('Api Calls', () => {
+describe('Daily Image Api Call', () => {
   const mockResponse = {
     hdurl: 'image_url_string',
     title: 'Nasa Image',
     explanation: 'This is a really great photo of a moon.'
-  }
+  };
 
   beforeEach(() => {
     window.fetch = jest.fn().mockImplementation(() => {
@@ -32,9 +32,36 @@ describe('Api Calls', () => {
   it('Should throw an error if there is a problem with the fetch', () => {
     expect(dailyImage()).rejects.toEqual('Error');
   });
+});
+
+describe('Daily Image On Date Api Call', () => {
+  const mockResponse = {
+    hdurl: 'image_url_string',
+    title: 'Nasa Image',
+    explanation: 'This is a really great photo of a moon.'
+  };
+
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      });
+    });
+  });
 
   it('Should return a response if fetch is ok', () => {
     const result = dailyImageOnDate('10/11/2019');
     expect(result).resolves.toEqual(mockResponse);
+  });
+
+  it('Should throw an error if the response is not ok', () => {
+    const response = 'There was an error fetching your photo';
+    window.fetch = jest.fn().mockImplementation(() => {
+      Promise.resolve({
+        ok: false
+      });
+    });
+    expect(dailyImageOnDate()).rejects.toEqual(response);
   })
 });
